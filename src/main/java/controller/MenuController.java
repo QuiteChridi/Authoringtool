227a39dca3f04adf0bbc7e099e7ce842eeb80082
Chris Zimmerer <chris.zimmerer@uni-wuerzburg.de>
2021-01-25 10:46:02 +0100

    init

---
+++ src/main/java/controller/MenuController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javafx.fxml.FXML;
+
+public class MenuController {
+
+    @FXML
+    void buttonPressed() {
+       System.out.println("Scene Builder ist ein tolles Programm um Scenes für JavaFX zu bauen!\nhttps://gluonhq.com/products/scene-builder/");
+    }
+
+
+}
