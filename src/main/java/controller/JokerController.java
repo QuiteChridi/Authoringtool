c000acc96329453acb758fad280085e175b5b207
antonliebe <anton.liebe@stud-mail.uni-wuerzburg.de>
2024-02-21 12:53:01 +0100

    Joker selcect Funktion

--- src/main/java/controller/JokerController.java
+++ src/main/java/controller/JokerController.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.Database;
 import model.Joker;
@@ -27,9 +28,19 @@ public class JokerController {
     private TableColumn<Joker, Integer>  priceColumn;
     @FXML
     private TableColumn<Joker, String> descriptionColumn;
+    @FXML
+            private TextField idField;
+    @FXML
+            private TextField nameField;
+    @FXML
+            private TextField priceField;
+    @FXML
+            private TextField descriptionField;
 
     ObservableList<Joker> jokerData= FXCollections.observableArrayList();
     Connection connection= Database.getInstance().conn;
+
+     Joker currentSelectedJoker;
 
     @FXML
     private void initialize(){
@@ -38,7 +49,7 @@ public class JokerController {
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("nameJoker"));
         priceColumn.setCellValueFactory(new PropertyValueFactory<>("costJoker"));
         descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("descriptionJoker"));
-
+        jokerTable.setFocusTraversable(false);
         loadJokerData();
     }
 
@@ -61,5 +72,15 @@ public class JokerController {
             e.printStackTrace();
         }
         jokerTable.setItems(jokerData);
+    }
+
+    @FXML
+    private void  select(){
+        idField.setStyle("-fx-text-fill: black");
+        currentSelectedJoker = jokerTable.getSelectionModel().getSelectedItem();
+        nameField.setText(currentSelectedJoker.getNameJoker());
+        idField.setText(String.valueOf(currentSelectedJoker.getIdJoker()));
+        priceField.setText(String.valueOf(currentSelectedJoker.getCostJoker()));
+        descriptionField.setText(currentSelectedJoker.getDescriptionJoker());
     }
 }
